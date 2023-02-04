7400eb6580dc40cdfa717b4dd9af34284c9e02b3
guleli0011 <oyunlartv11@gmail.com>
2023-02-04 10:52:39 +0400

    guleli0011

---
+++ index.js
@@ -0,0 +1,34 @@
+function opendiv1() {
+    document.getElementById("dropdownmenucon6").style.display = "flex";
+}
+  
+function closediv1() {
+    document.getElementById("dropdownmenucon6").style.display = "none";
+}
+
+function opendiv2() {
+    document.getElementById("mobilesearch1").style.display = "flex";
+}
+  
+function closediv2() {
+    document.getElementById("mobilesearch1").style.display = "none";
+}
+
+function shopcardopen1() {
+    document.getElementById("shopcard1").style.width = "350px";
+    document.getElementById("shopcard1").style.display = "block";
+}
+  
+function shopcardclose1() {
+    document.getElementById("shopcard1").style.width = "0px";
+    document.getElementById("shopcard1").style.display = "none";
+}
+function mobilemenuopen1() {
+    document.getElementById("mobilemenu1").style.width = "350px";
+    document.getElementById("mobilemenu1").style.display = "block";
+}
+  
+function mobilemenuclose1() {
+    document.getElementById("mobilemenu1").style.width = "0px";
+    document.getElementById("mobilemenu1").style.display = "none";
+}
